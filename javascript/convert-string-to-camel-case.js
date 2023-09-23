70ee66e43e72c04b1a14d490994292417923ea7d
Derek Wheeler <frozenarrow73@gmail.com>
2023-09-23 17:35:56 -0600

    Codewars solution to *Convert string to camel case*

--- javascript/convert-string-to-camel-case.js
+++ javascript/convert-string-to-camel-case.js
@@ -1,5 +1,24 @@
 //Codewars challenge "Convert string to camel case"
 
 function toCamelCase(str){
+    str = str.replaceAll("-", "_")
+    let arr = str.split("_")
+    for(let i = 1; i < arr.length; i++) {
+      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1)
+    }
+    str = arr.join("")
+    
+    return str
+  }
 
-}
+let test1 = "string-one"
+let test2 = "string_two"
+let test3 = "string_three-three"
+let test4 = "String-Four"
+let test5 = "string-five-five"
+
+console.log(toCamelCase(test1))
+console.log(toCamelCase(test2))
+console.log(toCamelCase(test3))
+console.log(toCamelCase(test4))
+console.log(toCamelCase(test5))
